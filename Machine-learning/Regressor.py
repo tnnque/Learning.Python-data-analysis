5bfe7bf47ecc4fc59a78d5dab042263fac95e170
tnnque <tnnque@gmail.com>
2020-02-22 09:16:51 +0700

    Working

--- Machine-learning/Regressor.py
+++ Machine-learning/Regressor.py
@@ -23,6 +23,7 @@ from sklearn import linear_model
 linear_regressor = linear_model.LinearRegression()
 linear_regressor.fit(X_train, y_train)
 
+# Plotting
 import matplotlib.pyplot as plt
 y_train_pred = linear_regressor.predict(X_train)
 plt.figure()
@@ -36,3 +37,22 @@ plt.scatter(X_test, y_test, color='red')
 plt.plot(X_test, y_test_pred, color='black', linewidth=4)
 plt.title('Test data')
 plt.show()
+
+# Computing regression accuracy
+import sklearn.metrics as sm
+print("Mean absolute error =", round(sm.mean_absolute_error(y_test, y_test_pred), 2))
+print("Mean squared error =", round(sm.mean_squared_error(y_test, y_test_pred), 2))
+print("Median absolute error =", round(sm.median_absolute_error(y_test, y_test_pred), 2))
+print("Explained variance score =", round(sm.explained_variance_score(y_test, y_test_pred), 2))
+print("R2 score = ", round(sm.r2_score(y_test, y_test_pred), 2))
+
+# Achieving model persistence
+import pickle
+output_model_file = 'saved_model.pk1'
+with open(output_model_file, 'wb') as f:
+    pickle.dump(linear_regressor, f)
+with open(output_model_file, 'rb') as f:
+    model_linregr = pickle.load(f)
+
+y_test_pred_new = model_linregr.predict(X_test)
+print("\nNew mean absolute error =", round(sm.mean_absolute_error(y_test, y_test_pred_new), 2))
