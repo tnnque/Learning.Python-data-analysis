c4d5ad2ece2dff381520cdbbb7133ff17a652bc9
tnnque <tnnque@gmail.com>
2020-02-24 19:55:19 +0700

    Working

--- Machine-learning/Regressor.py
+++ Machine-learning/Regressor.py
@@ -1,42 +1,44 @@
 # import sys
 import numpy as np
+import pandas as pd
 
-filename = '/Users/tnnque/PycharmProjects/Python-data-analysis/Machine-learning/data_singlevar.txt'
+filename = '/Users/tnnque/PycharmProjects/Python-data-analysis/Machine-learning/data_multivar.txt'
 X = []
 y = []
 with open(filename, 'r') as f:
     for line in f.readlines():
-        xt, yt = [float(i) for i in line.split(',')]
-        X.append(xt)
-        y.append(yt)
+        temp = [float(i) for i in line.split(',')]
+        X.append(temp[:-1])
+        y.append(temp[-1])
 
 num_training = int(0.8 * len(X))
 num_test = len(X) - num_training
 #___ Training data
-X_train = np.array(X[:num_training]).reshape((num_training, 1))
+X_train = np.array(X[:num_training]).reshape((num_training, 3))
 y_train = np.array(y[:num_training])
 #___ Test data
-X_test = np.array(X[num_training:]).reshape((num_test,1))
+X_test = np.array(X[num_training:]).reshape((num_test,3))
 y_test = np.array(y[num_training:])
 
 from sklearn import linear_model
 linear_regressor = linear_model.LinearRegression()
 linear_regressor.fit(X_train, y_train)
-
-# Plotting
-import matplotlib.pyplot as plt
 y_train_pred = linear_regressor.predict(X_train)
-plt.figure()
-plt.scatter(X_train, y_train, color='green')
-plt.plot(X_train, y_train_pred, color='black', linewidth=4)
-plt.title('Training data')
-plt.show()
-
 y_test_pred = linear_regressor.predict(X_test)
-plt.scatter(X_test, y_test, color='red')
-plt.plot(X_test, y_test_pred, color='black', linewidth=4)
-plt.title('Test data')
-plt.show()
+
+# # Plotting
+# import matplotlib.pyplot as plt
+# plt.figure()
+# plt.scatter(X_train, y_train, color='green')
+# plt.plot(X_train, y_train_pred, color='black', linewidth=4)
+# plt.title('Training data')
+# plt.show()
+#
+
+# plt.scatter(X_test, y_test, color='red')
+# plt.plot(X_test, y_test_pred, color='black', linewidth=4)
+# plt.title('Test data')
+# plt.show()
 
 # Computing regression accuracy
 import sklearn.metrics as sm
@@ -56,3 +58,25 @@ with open(output_model_file, 'rb') as f:
 
 y_test_pred_new = model_linregr.predict(X_test)
 print("\nNew mean absolute error =", round(sm.mean_absolute_error(y_test, y_test_pred_new), 2))
+
+# Building ridge regressor
+ridge_regressor = linear_model.Ridge(alpha=0.01, fit_intercept=True, max_iter=10000)
+ridge_regressor.fit(X_train, y_train)
+y_test_pred_ridge = ridge_regressor.predict(X_test)
+print("Mean absolute error =", round(sm.mean_absolute_error(y_test, y_test_pred_ridge), 2))
+print("Mean squared error =", round(sm.mean_squared_error(y_test, y_test_pred_ridge), 2))
+print("Median absolute error =", round(sm.median_absolute_error(y_test, y_test_pred_ridge), 2))
+print("Explained variance score =", round(sm.explained_variance_score(y_test, y_test_pred_ridge), 2))
+print("R2 score = ", round(sm.r2_score(y_test, y_test_pred_ridge), 2))
+
+# Building polynomial regressor
+from sklearn.preprocessing import PolynomialFeatures
+polynomial = PolynomialFeatures(degree=10)
+X_train_transformed = polynomial.fit_transform(X_train)
+datapoint = [[0.39, 2.78, 7.11]]
+poly_datapoint = polynomial.fit_transform(datapoint)
+
+poly_linear_model = linear_model.LinearRegression()
+poly_linear_model.fit(X_train_transformed, y_train)
+print("\nLinear regression:", linear_regressor.predict(datapoint)[0])
+print("\nPolynomial regression:", poly_linear_model.predict(poly_datapoint)[0])
