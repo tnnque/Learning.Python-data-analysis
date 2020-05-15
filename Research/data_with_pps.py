b3c6d7e89c8eb69694f14c65c71b6e175874e63c
tnnque <tnnque@gmail.com>
2020-05-15 16:33:20 +0700

    Added polynomial Working on PCA for visualization

--- Research/data_with_pps.py
+++ Research/data_with_pps.py
@@ -42,3 +42,104 @@ sns.heatmap(round(correlation,5), annot=True, ax=ax1, cmap="coolwarm", linewidth
 fig1.subplots_adjust(top=0.5)
 t1= fig1.suptitle('Wine Attributes Correlation Heatmap', fontsize=14)
 plt.show()
+
+classifiers = {}
+parameters = {}
+
+# Create and update dictionary with Extra Trees Ensemble
+classifiers.update({"Extra Trees Ensemble": ExtraTreesClassifier()})
+parameters.update({"Extra Trees Ensemble": {
+                                            'classifier__n_estimators': [200],
+                                            'classifier__class_weight': [None, 'balanced'],
+                                            'classifier__max_features': ['auto', 'sqrt', 'log2'],
+                                            'classifier__max_depth': [3, 4, 5, 6, 7, 8],
+                                            'classifier__min_samples_split': [0.005, 0.01, 0.05, 0.10],
+                                            'classifier__min_samples_leaf': [0.005, 0.01, 0.05, 0.10],
+                                            'classifier__criterion':['gini', 'entropy']     ,
+                                            'classifier__n_jobs': [-1]
+                                             }})
+
+# Create and update dictionary with Gradient Boosting
+classifiers.update({"Gradient Boosting": GradientBoostingClassifier()})
+parameters.update({"Gradient Boosting": {
+                                        'classifier__learning_rate':[0.15,0.1,0.05,0.01,0.005,0.001],
+                                        'classifier__n_estimators': [200],
+                                        'classifier__max_depth': [2,3,4,5,6],
+                                        'classifier__min_samples_split': [0.005, 0.01, 0.05, 0.10],
+                                        'classifier__min_samples_leaf': [0.005, 0.01, 0.05, 0.10],
+                                        'classifier__max_features': ['auto','sqrt', 'log2'],
+                                        'classifier__subsample': [0.8, 0.9, 1]
+                                         }})
+
+# Create and update dictionary with Random Forest Classifier
+classifiers.update({"Random Forest": RandomForestClassifier()})
+parameters.update({"Random Forest": {
+                                    'classifier__n_estimators': [200],
+                                    'classifier__class_weight': [None, 'balanced'],
+                                    'classifier__max_features': ['auto', 'sqrt', 'log2'],
+                                    'classifier__max_depth': [3, 4, 5, 6, 7, 8],
+                                    'classifier__min_samples_split': [0.005, 0.01, 0.05, 0.10],
+                                    'classifier__min_samples_leaf': [0.005, 0.01, 0.05, 0.10],
+                                    'classifier__criterion':['gini', 'entropy'],
+                                    'classifier__n_jobs': [-1]
+                                     }})
+                                    # this "Classifier Hyper-parameters" method is sourced from Frank Ceballos at https://gist.githubusercontent.com/frank-ceballos/0c7f2ecdb15854ff6b5e88af4bd01c41/raw/a5b8bed155b511c00dd8328d66fd1deadcbdf9b6/05ModelDesign&Selection.py
+
+# ### Start the work with some 'feature importance' classifiers
+# Create a dictionary for the classifiers
+feature_importance = {'Extra Trees Ensemble', 'Gradient Boosting', 'Random Forest'}
+
+def select_classifier(classifier_name):
+    # Input the classifier we want to use from dictionary
+    classifier = classifiers[classifier_name]
+
+    # Scale features
+    feature_scaler = preprocessing.StandardScaler()
+
+    # Steps for pipeline and grid
+    steps = [('scaler', feature_scaler), ('classifier', classifier)]
+    pipeline = Pipeline(steps=steps)
+    grid = parameters[classifier_name]
+
+    # Create grid search object
+    grid_search = GridSearchCV(pipeline, grid, cv=5, n_jobs=-1, verbose=1, scoring='roc_auc')
+    # Hieu fix
+    from sklearn.preprocessing import LabelEncoder
+    ohe = LabelEncoder()
+    labels = np.array(y_train)
+    ohe.fit(labels)
+    labels = ohe.transform(np.array(labels))
+    grid_search.fit(X_train, np.ravel(labels))
+
+    # Get final parameters and score
+    result_params = grid_search.best_params_
+    result_score = grid_search.best_score_
+
+    # Update classifier parameters
+    tuned_params = {item[12:]: result_params[item] for item in result_params}
+    classifier.set_params(**tuned_params)
+
+    return classifier, tuned_params, feature_scaler
+
+classifier, tuned_params, feature_scaler = select_classifier('Random Forest')
+
+# Select Features using RFECV
+class PipelineRFE(Pipeline):
+    # Source: https://ramhiser.com/post/2018-03-25-feature-selection-with-scikit-learn-pipeline/
+    def fit(self, X, y=None, **fit_params):
+        super(PipelineRFE, self).fit(X, y, **fit_params)
+        self.feature_importances_ = self.steps[-1][-1].feature_importances_
+        return self
+# Define pipeline for RFECV
+steps = [('scaler', feature_scaler), ('classifier', classifier)]
+pipe = PipelineRFE(steps = steps)
+
+# Initialize RFECV object
+feature_selector = RFECV(pipe, cv = 5, step = 1, scoring = 'roc_auc', verbose = 1)
+
+# Fit RFECV
+feature_selector.fit(X_train, np.ravel(y_train))
+
+# Get selected features
+feature_names = X_train.columns
+selected_features = feature_names[feature_selector.support_].tolist()
--- Side-projects/red_wine_quality.py
+++ Side-projects/red_wine_quality.py
@@ -1,58 +1,15 @@
-import math, statistics
-import csv
+import math
+import warnings
+warnings.filterwarnings('ignore')
 import numpy as np
 import pandas as pd
-import ppscore as pps
-from sklearn.linear_model import Ridge
-from sklearn.linear_model import Lasso
-from sklearn.pipeline import Pipeline
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, BaggingClassifier, ExtraTreesClassifier, GradientBoostingClassifier
-from sklearn import preprocessing, linear_model
-import sklearn.metrics as sm
 import matplotlib.pyplot as plt
 import seaborn as sns
+import sklearn.metrics as sm
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.ensemble import RandomForestRegressor
+from sklearn import preprocessing, linear_model
 
-classifiers = {}
-parameters = {}
-
-# Create and update dictionary with Gradient Boosting
-classifiers.update({"Gradient Boosting": GradientBoostingClassifier()})
-parameters.update({"Gradient Boosting": {
-                                        "classifier__learning_rate":[0.15,0.1,0.05,0.01,0.005,0.001],
-                                        "classifier__n_estimators": [200],
-                                        "classifier__max_depth": [2,3,4,5,6],
-                                        "classifier__min_samples_split": [0.005, 0.01, 0.05, 0.10],
-                                        "classifier__min_samples_leaf": [0.005, 0.01, 0.05, 0.10],
-                                        "classifier__max_features": ["auto", "sqrt", "log2"],
-                                        "classifier__subsample": [0.8, 0.9, 1]
-                                         }})
-
-# Create and update dictionary with Extra Trees Ensemble
-classifiers.update({"Extra Trees Ensemble": ExtraTreesClassifier()})
-parameters.update({"Extra Trees Ensemble": {
-                                            "classifier__n_estimators": [200],
-                                            "classifier__class_weight": [None, "balanced"],
-                                            "classifier__max_features": ["auto", "sqrt", "log2"],
-                                            "classifier__max_depth" : [3, 4, 5, 6, 7, 8],
-                                            "classifier__min_samples_split": [0.005, 0.01, 0.05, 0.10],
-                                            "classifier__min_samples_leaf": [0.005, 0.01, 0.05, 0.10],
-                                            "classifier__criterion" :["gini", "entropy"]     ,
-                                            "classifier__n_jobs": [-1]
-                                             }})
-
-# Create and update dictionary with Random Forest Classifier
-classifiers.update({"Random Forest": RandomForestClassifier()})
-parameters.update({"Random Forest": {
-                                    "classifier__n_estimators": [200],
-                                    "classifier__class_weight": [None, "balanced"],
-                                    "classifier__max_features": ["auto", "sqrt", "log2"],
-                                    "classifier__max_depth" : [3, 4, 5, 6, 7, 8],
-                                    "classifier__min_samples_split": [0.005, 0.01, 0.05, 0.10],
-                                    "classifier__min_samples_leaf": [0.005, 0.01, 0.05, 0.10],
-                                    "classifier__criterion" :["gini", "entropy"]     ,
-                                    "classifier__n_jobs": [-1]
-                                     }})
 
 # Define dataset loading function
 def data_load(filename):
@@ -64,13 +21,13 @@ def data_load(filename):
 
 # Input and separate X, y for training and testing data
 df, X, y = data_load('winequality-red.csv')
-# data_scaler = preprocessing.MinMaxScaler(feature_range=(0, 10))
-# X = pd.DataFrame(data_scaler.fit_transform(X), columns=df.columns[:11])
+data_scaler = preprocessing.MinMaxScaler(feature_range=(0, 10))
+X = pd.DataFrame(data_scaler.fit_transform(X), columns=df.columns[:11])
 feature_names = X.columns
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.2)
 
 ## Take a look at the features
-#Density plot and histogram (see features'distribution)
+# Density plot and histogram (see features'distribution)
 for i in X:
     sns.distplot(X[i], hist=True, kde=True, bins=10, color='red',
         hist_kws={'color':'gray', 'edgecolor':'black'},
@@ -84,26 +41,22 @@ plt.show()
 # Use correlation matrix to detect highly correlated features
 correlation = X_train.corr(method='spearman').abs()
 
-fig, ax = plt.subplots(figsize=(10, 6))
-sns.heatmap(round(correlation,5), square=True, ax=ax, cmap="Blues", linewidths=.5)
-fig.tight_layout()
-plt.show()
+# fig, ax = plt.subplots(figsize=(10, 6))
+# sns.heatmap(round(correlation,5), square=True, ax=ax, cmap="Blues", linewidths=.5)
+# fig.tight_layout()
+# plt.show()
 
 # Remove feature columns where correlation > 0.95
 up_triangle = correlation.where(np.triu(np.ones(correlation.shape), k = 1).astype(np.bool))
-remv = [column for column in up_triangle.columns if any(up_triangle[column] > 0.95)]
+remv = [column for column in up_triangle.columns if any(up_triangle[column] > 0.9)]
 
 X_train = X_train.drop(remv, axis = 1)
 X_test = X_test.drop(remv, axis = 1)
 
-
-# ### Start the work with some 'feature importance' classifiers
-# Create a dictionary for the classifiers
-FEATURE_IMPORTANCE = {"Gradient Boosting", "Extra Trees Ensemble", "Random Forest"}
-
-# # Train the random forest regressor
-# rf_regressor = RandomForestRegressor(n_estimators=1000, max_depth=10, min_impurity_decrease=0)
-# rf_regressor.fit(X_train, y_train)
+# ======================================================================
+# Train random forest regressor
+rf_regressor = RandomForestRegressor(n_estimators=1000, max_depth=10, min_impurity_decrease=0)
+rf_regressor.fit(X_train, y_train)
 
 # Define feature importances plotting function
 def plot_importances(regressor, title, feature_names):
@@ -143,9 +96,7 @@ y_pred_rf = rf_regressor.predict(X_test)
 print("RF Mean squared error =", round(sm.mean_squared_error(y_test, y_pred_rf), 5))
 print("RF Explained variance score =", round(sm.explained_variance_score(y_test, y_pred_rf), 5))
 
-##
-
-======================================================================
+# ======================================================================
 # Train multivariate linear regressor
 ln_regressor = linear_model.LinearRegression()
 ln_regressor.fit(X_train, y_train)
@@ -160,3 +111,41 @@ mse = round(sm.mean_squared_error(y_test, y_pred_ln), 5)
 rmse = round(math.sqrt(mse), 5)
 print("LN mean squared error =", mse)
 print("LN root mean squared error =", rmse)
+
+# ======================================================================
+# Train polynomial regressor
+
+polynomial = preprocessing.PolynomialFeatures(degree=2)
+X_train_poly = polynomial.fit_transform(X_train)
+X_test_poly = polynomial.fit_transform(X_test)
+pl_regressor = linear_model.LinearRegression()
+pl_regressor.fit(X_train_poly, y_train)
+
+print('PL training score: {}'.format(pl_regressor.score(X_train_poly, y_train)))
+print('PL test score: {}'.format(pl_regressor.score(X_test_poly, y_test)))
+
+# Evaluate LN regressor's performance
+y_pred_pl = pl_regressor.predict(X_test_poly)
+mse = round(sm.mean_squared_error(y_test, y_pred_pl), 5)
+rmse = round(math.sqrt(mse), 5)
+print("PL mean squared error =", mse)
+print("PL root mean squared error =", rmse)
+
+# ======================================================================
+# Train logistic regression classirifier
+lg_classifier = linear_model.LogisticRegression(solver='liblinear', C=100)
+lg_classifier.fit(X_train, y_train)
+
+y_pred_lg = lg_classifier.predict(X_train)
+f1 = cross_val_score(lg_classifier, X, y, scoring='f1_weighted', cv=5)
+print("F1:" + str(round(f1.mean(), 5)))
+
+# Evaluate RF regressor's performance
+print('LG training Score: {}'.format(lg_classifier.score(X_train, y_train)))
+print('LG test Score: {}'.format(lg_classifier.score(X_test, y_test)))
+
+### The regressors'results are not as good as expected, need to check for the shape of these features.
+#### Because now our data has 11 dimensions, which is not good for visualization, PCA needs to be applied first.
+# ======================================================================
+# Apply PCA
+
